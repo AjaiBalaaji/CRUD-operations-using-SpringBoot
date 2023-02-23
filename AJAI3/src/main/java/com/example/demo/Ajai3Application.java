89a5e89228240619e86319b4ebfd5c384cf35c9b
AjaiBalaaji <113657223+AjaiBalaaji@users.noreply.github.com>
2023-02-23 23:03:01 +0530

    Add files via upload

---
+++ AJAI3/src/main/java/com/example/demo/Ajai3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ajai3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ajai3Application.class, args);
+	}
+
+}
